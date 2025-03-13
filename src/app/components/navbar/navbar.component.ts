b91a0e227345bbd1ab0dd8eaf00cfedf27ab5c16
AbhiganBanerjee <abhiganbanerjee1@gmail.com>
2025-03-13 21:24:52 +0530

    5th Commit Working on Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  //Perform Constructor Injection for the BreakpointObserver service to track the Device Responsive ness
+  constructor(private breakPointObserver:BreakpointObserver){}
 
+  //Define a boolean flag to track the Small Screen status
+  public isSmallScreen:boolean = false;
+
+  ngOnInit(): void {
+    //Call the observe method on the observer ref, to track the device [Handset] responsive
+    this.breakPointObserver.observe([Breakpoints.Handset]).subscribe((result)=>{
+      this.isSmallScreen = result.matches;
+    });
+  }
 }
