b3fbaf564f4e67f8b61086d2e1e82b044cf48296
AbhiganBanerjee <abhiganbanerjee1@gmail.com>
2025-06-29 21:14:58 +0530

    Home component asideListitems array Spring API waiting

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  /*Initialize a list of objects containing social media icons as aside element items
+    the List of aside item objects will come from Spring Boot API
+  */
+  public asideItems:any = [
+    
+  ]
 }
